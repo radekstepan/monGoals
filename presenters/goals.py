745888edd2398ea4432a12e9b571680de281e121
Radek <radek.stepan@gmail.com>
2011-04-13 22:39:16 +0100

    new goal dict formation

--- presenters/goals.py
+++ presenters/goals.py
@@ -8,16 +8,52 @@ from flask.helpers import url_for
 
 from models.goals import Goals
 
+import time
+from datetime import date
+
 goals = Module(__name__)
 
 @goals.route('/')
 def all():
     return render_template('main.html', elmo="elmo")
 
-@goals.route('/new')
+@goals.route('/new', methods=['GET', 'POST'])
 def new():
-    fibonacci = [1,2,3,5,8,13,20]
-    return render_template('new.html', **locals())
+    if request.method == 'POST':
+        goal = {'name': request.form['name'], 'description': request.form['description']}
+
+        goal['date'] = {'begin': date.today(), 'end': date(
+                int(request.form['due-date[year]']),
+                int(request.form['due-date[month]']),
+                int(request.form['due-date[day]']))}
+
+        goal['variant'] = request.form['variant']
+
+        if goal['variant'] == 'points':
+            points = {
+                'target': request.form['points[target]'],
+                'currency': [] }
+
+            for n in range(10):
+                if 'points[name-'+str(n)+']' in request.form and len(request.form['points[name-'+str(n)+']']) > 0:
+                    currency = {
+                        'name': request.form['points[name-'+str(n)+']'],
+                        'points': int(request.form['points[currency-'+str(n)+']']) }
+                    points['currency'].append(currency)
+
+            goal['points'] = points
+            
+        else:
+            goal['target'] = {
+                'current': request.form['target[current]'],
+                'end': request.form['target[end]'] }
+
+        print goal
+
+        return redirect(url_for('goals.goal'))
+    else:
+        fibonacci = [1,2,3,5,8,13,20]
+        return render_template('new.html', **locals())
 
 @goals.route('/goal')
 def goal():
