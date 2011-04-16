5b0443377512756eabc925e349262c468bf7e0fd
Radek <radek.stepan@gmail.com>
2011-04-16 20:01:29 +0100

    progress chart working

--- app.py
+++ app.py
@@ -32,6 +32,22 @@ def create_app(database, drop=False):
     def timestamp_format(timestamp):
         return utils.timestamp_format(timestamp)
 
+    @app.template_filter('timestamp_distance')
+    def timestamp_distance(timestamp):
+        r = (utils.timestamp_new() - timestamp)/60/60/24
+        if r > 0:
+            if r > 1:
+                return ''.join([str(r), ' days ago'])
+            else:
+                return 'yesterday'
+        elif r < 0:
+            if r < -1:
+                return ''.join(['in ', str(abs(r)), ' days'])
+            else:
+                return 'tomorrow'
+        else:
+            return 'today'
+
     return app
 
 if __name__ == '__main__':
