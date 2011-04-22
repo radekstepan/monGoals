7a084fb0e3a47f60498e89915ca0231f06927e45
Radek <radek.stepan@gmail.com>
2011-04-22 22:44:00 +0100

    simplistic new form validation

--- presenters/goals.py
+++ presenters/goals.py
@@ -32,62 +32,85 @@ def all():
 def new():
     '''create new goal'''
     if request.method == 'POST':
-        # meta
-        goal = {'name': request.form['name'], 'description': request.form['description']}
+        # validate
+        error = []
+        if 'name' not in request.form or not request.form['name']: error.append('Goal name is missing')
+        if 'file' not in request.files or not request.files['file']: error.append('Reward image file is missing')
 
-        # dates
-        goal['date'] = {
-            'begin': utils.timestamp_new(),
-            'end': utils.timestamp_new(
-                    year = request.form['due-date[year]'],
-                    month = request.form['due-date[month]'],
-                    day = request.form['due-date[day]'])
-        }
+        if not error:
+            # meta
+            goal = {'name': request.form['name'], 'description': request.form['description']}
 
-        goal['reward'] = utils.file_to_mongo(request.files['file'])
-        goal['variant'] = request.form['variant']
-        goal['log'] = []
-
-        # points system
-        if goal['variant'] == 'points':
-            points = {
-                'target': int(request.form['points[target]']),
-                'logged': 0,
-                'currency': [] }
-            
-            for n in range(10):
-                if 'points[name-'+str(n)+']' in request.form and len(request.form['points[name-'+str(n)+']']) > 0:
-                    currency = {
-                        'name': request.form['points[name-'+str(n)+']'],
-                        'points': int(request.form['points[currency-'+str(n)+']']) }
-                    points['currency'].append(currency)
-
-            goal['points'] = points
-
-        # target value system
-        else:
-            goal['values'] = {
-                'begin': float(request.form['values[begin]']),
-                'end': float(request.form['values[end]']),
-                'current': float(request.form['values[begin]'])
-            }
-            goal['points'] = {
-                'target': int(request.form['target[points]']),
-                'logged': 0,
-                'conversion': (float(request.form['values[end]']) - float(request.form['values[begin]'])) / float(request.form['target[points]']),
-                'unit': request.form['unit']
+            # dates
+            goal['date'] = {
+                'begin': utils.timestamp_new(),
+                'end': utils.timestamp_new(
+                        year = request.form['due-date[year]'],
+                        month = request.form['due-date[month]'],
+                        day = request.form['due-date[day]'])
             }
 
-        # save
-        g = Goals()
-        id = g.save(goal)
+            goal['reward'] = utils.file_to_mongo(request.files['file'])
+            goal['variant'] = request.form['variant']
+            goal['log'] = []
 
-        return redirect(url_for('goals.goal', id=id))
-    else:
-        g = Goals()
-        goals = utils.sort_list_by_points(g.to_list(g.find_all()))
-        fibonacci = [1,2,3,5,8,13,20]
-        return render_template('new.html', **locals())
+            # points system
+            if goal['variant'] == 'points':
+                # validate
+                if 'points[target]' not in request.form or not request.form['points[target]']:
+                    error.append('Target amount of points is missing')
+                if 'points[name-0]' not in request.form or not request.form['points[name-0]']:
+                    error.append('At least one currency activity is missing')
+
+                if not error:
+                    points = {
+                        'target': int(request.form['points[target]']),
+                        'logged': 0,
+                        'currency': [] }
+
+                    for n in range(10):
+                        if 'points[name-'+str(n)+']' in request.form and len(request.form['points[name-'+str(n)+']']) > 0:
+                            currency = {
+                                'name': request.form['points[name-'+str(n)+']'],
+                                'points': int(request.form['points[currency-'+str(n)+']']) }
+                            points['currency'].append(currency)
+
+                    goal['points'] = points
+
+            # target value system
+            else:
+                # validate
+                if 'values[begin]' not in request.form or not request.form['values[begin]']:
+                    error.append('A starting value is missing')
+                if 'values[end]' not in request.form or not request.form['values[end]']:
+                    error.append('A target value is missing')
+                if 'target[points]' not in request.form or not request.form['target[points]']:
+                    error.append('Target amount of points is missing')
+
+                if not error:
+                    goal['values'] = {
+                        'begin': float(request.form['values[begin]']),
+                        'end': float(request.form['values[end]']),
+                        'current': float(request.form['values[begin]'])
+                    }
+                    goal['points'] = {
+                        'target': int(request.form['target[points]']),
+                        'logged': 0,
+                        'conversion': (float(request.form['values[end]']) - float(request.form['values[begin]'])) / float(request.form['target[points]']),
+                        'unit': request.form['unit']
+                    }
+
+            if not error:
+                # save
+                g = Goals()
+                id = g.save(goal)
+
+                return redirect(url_for('goals.goal', id=id))
+
+    g = Goals()
+    goals = utils.sort_list_by_points(g.to_list(g.find_all()))
+    fibonacci = [1,2,3,5,8,13,20]
+    return render_template('new.html', **locals())
 
 @goals.route('/goal/<id>')
 def goal(id):
